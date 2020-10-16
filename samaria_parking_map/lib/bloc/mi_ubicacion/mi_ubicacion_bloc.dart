f7cef5e25678aec22699ed6af1a568e667f9c78f
andresperezfc <felipe3apc@gmail.com>
2020-10-16 10:41:28 -0500

    Seguir ubicación del usuario

--- samaria_parking_map/lib/bloc/mi_ubicacion/mi_ubicacion_bloc.dart
+++ samaria_parking_map/lib/bloc/mi_ubicacion/mi_ubicacion_bloc.dart
@@ -1,13 +1,30 @@
 import 'dart:async';
 
 import 'package:bloc/bloc.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:meta/meta.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geolocator/geolocator.dart' as Geolocator;
+import 'package:google_maps_flutter/google_maps_flutter.dart' show LatLng;
 part 'mi_ubicacion_event.dart';
 part 'mi_ubicacion_state.dart';
 
 class MiUbicacionBloc extends Bloc<MiUbicacionEvent, MiUbicacionState> {
   MiUbicacionBloc() : super(MiUbicacionState());
+
+  StreamSubscription<Position> _positionSubscription;
+
+  void iniciarSeguimiento() {
+    _positionSubscription = Geolocator.getPositionStream(
+            desiredAccuracy: Geolocator.LocationAccuracy.high,
+            distanceFilter: 10)
+        .listen((Geolocator.Position position) {
+      print(position);
+    });
+  }
+
+  void cancelarSeguimiento() {
+    _positionSubscription?.cancel();
+  }
 
   @override
   Stream<MiUbicacionState> mapEventToState(
--- samaria_parking_map/lib/pages/mapa_page.dart
+++ samaria_parking_map/lib/pages/mapa_page.dart
@@ -1,6 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:samaria_parking_map/bloc/mi_ubicacion/mi_ubicacion_bloc.dart';
 
-class MapaPage extends StatelessWidget {
+class MapaPage extends StatefulWidget {
+  @override
+  _MapaPageState createState() => _MapaPageState();
+}
+
+class _MapaPageState extends State<MapaPage> {
+  @override
+  void initState() {
+    context.bloc<MiUbicacionBloc>().iniciarSeguimiento();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    context.bloc<MiUbicacionBloc>().cancelarSeguimiento();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
