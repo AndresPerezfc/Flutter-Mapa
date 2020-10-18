63f473ef88f963de19049f534f639028d97ecbfb
andresperezfc <felipe3apc@gmail.com>
2020-10-18 10:18:20 -0500

    Diseño: marcador central y opciones

---
+++ samaria_parking_map/lib/widgets/marcador_manual.dart
@@ -0,0 +1,48 @@
+part of 'widgets.dart';
+
+class MarcadorManual extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+    return Stack(
+      children: [
+        Positioned(
+            top: 70,
+            left: 20,
+            child: CircleAvatar(
+              maxRadius: 25,
+              backgroundColor: Colors.white,
+              child: IconButton(
+                icon: Icon(
+                  Icons.arrow_back,
+                  color: Color(0xff5abd8c),
+                ),
+                onPressed: () {},
+              ),
+            )),
+        Center(
+          child: Transform.translate(
+              offset: Offset(0, -20),
+              child: Icon(
+                Icons.location_on,
+                size: 50,
+                color: Color(0xff5abd8c),
+              )),
+        ),
+        Positioned(
+            bottom: 70,
+            left: 40,
+            child: MaterialButton(
+              minWidth: width - 120,
+              child: Text('Confirmar destino',
+                  style: TextStyle(color: Colors.white)),
+              color: Color(0xff5abd8c),
+              elevation: 1,
+              shape: StadiumBorder(),
+              splashColor: Colors.transparent,
+              onPressed: () {},
+            )),
+      ],
+    );
+  }
+}
